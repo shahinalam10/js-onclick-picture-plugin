e4b4db69f0285b3cc4984eea856ca5454e4112a2
md shahin alam <shahin15-12140@diu.edu.bd>
2023-04-19 12:32:23 +0600

    js onlclick picture plugin

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var mainImage = document.getElementById('mainImage');
+var img1 = document.getElementById('img1');
+
+img1.onclick = function(){
+    mainImage.setAttribute(('src'), this.getAttribute('src'));
+}
+img2.onclick = function(){
+    mainImage.setAttribute(('src'), this.getAttribute('src'));
+}
+img3.onclick = function(){
+    mainImage.setAttribute(('src'), this.getAttribute('src'));
+}
+img4.onclick = function (){
+    mainImage.setAttribute(('src'), this.getAttribute('src'));
+}
+img5.onclick = function (){
+    mainImage.getAttribute(('src'), this.getAttribute('src'));
+}
